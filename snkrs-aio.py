a28e144a6cef56c43545dce6913821642466395d
zyx898 <55955228+zyx898@users.noreply.github.com>
2020-01-05 21:37:30 -0600

    Updated With Stock and Log
    Update much more infos

--- snkrs-aio.py
+++ snkrs-aio.py
@@ -1,146 +1,154 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-
-import requests,threading,random
 import json
-import re
 import time
-import datetime
 import random
-import pytz
+from datetime import datetime
+import requests
+import threading
 from discord_webhook import DiscordEmbed, DiscordWebhook
 
-webhook_url = '' # Put your discord Webhook
+headers= {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36'}
+
+webhookUrl = '' #Webhook To Post Pings 
+logWebhookurl = '' #Webhook To store Monitor Logs
 
 
-urls = ['https://api.nike.com/product_feed/threads/v2?filter=marketplace(US)&filter=language(en)&filter=channelId(008be467-6c78-4079-94f0-70e2d6cc4003)&fields=id&fields=lastFetchTime&fields=productInfo&fields=publishedContent.properties','https://api.nike.com/product_feed/threads/v2?filter=marketplace(CN)&filter=language(zh-Hans)&filter=channelId(008be467-6c78-4079-94f0-70e2d6cc4003)&fields=id&fields=lastFetchTime&fields=productInfo&fields=publishedContent.properties','https://api.nike.com/product_feed/threads/v2?filter=marketplace(GB)&filter=language(en-gb)&filter=channelId(008be467-6c78-4079-94f0-70e2d6cc4003)&fields=id&fields=lastFetchTime&fields=productInfo&fields=publishedContent.properties','https://api.nike.com/product_feed/threads/v2?filter=marketplace(JP)&filter=language(ja)&filter=channelId(008be467-6c78-4079-94f0-70e2d6cc4003)&fields=id&fields=lastFetchTime&fields=productInfo&fields=publishedContent.properties']
+def post_discord(imageUrl,title,status,styleColor,publishType,exclusiveAccess,hardLaunch,price,availability,method,Name,sizes):
+    info = (f'Method: {method}   |   Type: {publishType}   |\n Price: {price}   |   Hard Launch: {hardLaunch}   |')
+    webhook = DiscordWebhook(url=webhookUrl,content='')
+    embed = DiscordEmbed(title=f'{title} [ {status} ]',url='https://www.nike.com/launch/t/'+str(title), color=0x36393F, description=f' {Name} - {styleColor}\n  |  Avilable: {str(availability)}  | Exclusive: {exclusiveAccess}')
+    embed.add_embed_field(name='Info', value=info)
+    embed.add_embed_field(name='Stock', value='\n'.join(sizes),inline=False)
+    embed.set_author(name='@zyx898',icon_url='https://pbs.twimg.com/profile_images/1118878674642714624/lNXTIWNT_400x400.jpg',url='https://twitter.com/zyx898')
+    embed.set_thumbnail(url=imageUrl)
+    embed.set_footer(text=Name+' | SkrNotify', icon_url='https://pbs.twimg.com/profile_images/1134245182738718721/N12NVkrt_400x400.jpg')
+    embed.set_timestamp()
+    webhook.add_embed(embed)
+    webhook.execute()
 
-def gettime():
-	now = str(datetime.datetime.now())
-	
-	now = now.split(' ')[1]
-	
-	threadname = threading.currentThread().getName()
-	
-	threadname = str(threadname).replace('Thread', 'Task')
-	
-	now = '[' + str(now) + ']' + '[' + str(threadname) + ']'
+def post_log(message,Name):
+    webhook = DiscordWebhook(url=webhookUrl,content='')
+    embed = DiscordEmbed(title=Name,url='https://www.nike.com/launch/t/', color=0x36393F, description='')
+    embed.add_embed_field(name='Message', value=message)
+    embed.set_author(name='@zyx898',icon_url='https://pbs.twimg.com/profile_images/1118878674642714624/lNXTIWNT_400x400.jpg',url='https://twitter.com/zyx898')
+    embed.set_footer(text=Name+' | SkrNotify', icon_url='https://pbs.twimg.com/profile_images/1134245182738718721/N12NVkrt_400x400.jpg')
+    embed.set_timestamp()
+    webhook.add_embed(embed)
+    webhook.execute()
 
-	return now
+def scrpaeSlugs(url):
+    apiReq = requests.get(url,headers=headers)
+    apiInfo = json.loads(apiReq.text)
+    apiObjects =  apiInfo['objects']
+    objectSlugs = []
+    for item in apiObjects:
+        objectSlugs.append(item['publishedContent']['properties']['seo']['slug'])
+    return objectSlugs
 
-def message_post(webhook_url,title,image,restricrt,method,starttime):
-	try:
-		
-		webhook = DiscordWebhook(url=webhook_url,content='')
-		
-		embed = DiscordEmbed(title=title, color=0x00fea9,url="https://nike.com/launch")
-		
-		
-		embed.add_embed_field(name='**Product Infos：**',value='Access: '+restricrt +'\n\n'+'Start Time: '+starttime +'\n\n'+'Release Type: '+method)
-		
-		
-		embed.set_thumbnail(url=image)
-		
-		embed.set_footer(text='@zyx898',icon_url='https://pbs.twimg.com/profile_images/1118878674642714624/lNXTIWNT_400x400.jpg')
-		
-		embed.set_timestamp()
-		
-		webhook.add_embed(embed)
-		
-		webhook.execute()
-		
-		print(gettime() + '[SUCCESS] --> Successfully sent success webhook!')
-	except:
-		print(gettime() + '[ERROR] --> Unable to send webhook')
-		pass
+def hitApi(url):
+    apiReq = requests.get(url,headers=headers)
+    apiInfo = json.loads(apiReq.text)
+    apiObjects =  apiInfo['objects']
+    return apiObjects
 
+def main(API,Name):
+    old_slugs = scrpaeSlugs(API)
+    post_log(f'{Name} Monitor has started.',Name)
+    while True:
+        try:
+            apiObjects = hitApi(API)
+            new_slugs = []
+            for item in apiObjects:
+                new_slugs.append(item['publishedContent']['properties']['seo']['slug'])
+            differnces = set(new_slugs) - set(old_slugs)
+            if differnces:
+                for difference in differnces:
+                    for item in apiObjects:
+                        if item['publishedContent']['properties']['seo']['slug'] == difference:
+                            imageUrl = item['publishedContent']['properties']['coverCard']['properties']['squarishURL']
+                            title = item['publishedContent']['properties']['seo']['slug'].upper()
+                            try:
+                                status = item['productInfo'][0]['merchProduct']['status']
+                            except Exception as e:
+                                print('Error Parsing Status')
+                                status = 'Null'
+                                
+                            try:
+                                styleColor = item['productInfo'][0]['merchProduct']['styleColor']
+                            except Exception as e:
+                                print('Error Parsing Style Color')
+                                styleColor = 'Null'
+                                
+                            try:
+                                publishType = item['productInfo'][0]['merchProduct']['publishType']
+                            except Exception as e:
+                                print('Error Parsing Publish Type')
+                                publishType = 'Null'
+                                
+                            try:
+                                exclusiveAccess = item['productInfo'][0]['merchProduct']['exclusiveAccess']
+                            except Exception as e:
+                                print('Error Parsing Exclusive Access')
+                                exclusiveAccess = 'Null'
+                                
+                            try:
+                                hardLaunch = item['productInfo'][0]['merchProduct']['hardLaunch']
+                            except Exception as e:
+                                print('Error Parsing hard Launching')
+                                hardLaunch = 'Null'
+                            
+                            try:
+                                price = str(item['productInfo'][0]['merchPrice']['fullPrice']) +' USD'
+                            except Exception as e:
+                                print('Error Parsing Price')
+                                price = 'Nulll'
+                            
+                            try:
+                                availability = str(item['productInfo'][0]['availability']['available'])
+                            except Exception as e:
+                                print('Error Parsing Aviabality')
+                                availability = 'Null'
+                                
+                                
+                            try:
+                                method = item['productInfo'][0]['launchView']['method']
+                            except Exception as e:
+                                print('Error Parsing Method')
+                                method = 'Null'
+                                
+                            sizes = []
+                            try:
 
-def main(url):
-	while True:
-		try:
-			if url == 'https://api.nike.com/product_feed/threads/v2?filter=marketplace(US)&filter=language(en)&filter=channelId(008be467-6c78-4079-94f0-70e2d6cc4003)&fields=id&fields=lastFetchTime&fields=productInfo&fields=publishedContent.properties':
-				file1 = 'old_data_us.txt'
-			elif url == 'https://api.nike.com/product_feed/threads/v2?filter=marketplace(CN)&filter=language(zh-Hans)&filter=channelId(008be467-6c78-4079-94f0-70e2d6cc4003)&fields=id&fields=lastFetchTime&fields=productInfo&fields=publishedContent.properties':
-				file1 = 'old_data_cn.txt'
-			elif url == 'https://api.nike.com/product_feed/threads/v2?filter=marketplace(GB)&filter=language(en-gb)&filter=channelId(008be467-6c78-4079-94f0-70e2d6cc4003)&fields=id&fields=lastFetchTime&fields=productInfo&fields=publishedContent.properties':
-				file1 = 'old_data_gb.txt'
-			elif url == 'https://api.nike.com/product_feed/threads/v2?filter=marketplace(JP)&filter=language(ja)&filter=channelId(008be467-6c78-4079-94f0-70e2d6cc4003)&fields=id&fields=lastFetchTime&fields=productInfo&fields=publishedContent.properties':
-				file1 = 'old_data_jp.txt'
+                                for size,stock in zip(item['productInfo'][0]['skus'],item['productInfo'][0]['availableSkus']):
+                                    sizes.append(size['countrySpecifications'][0]['localizedSize'] +' '+ str(stock['level']))
+                                    
+                            except Exception as e:
+                                print('Error Getting Size and Stock')
+                                sizes = ['NUll']
+                                
+                            post_discord(imageUrl,title,status,styleColor,publishType,exclusiveAccess,hardLaunch,price,availability,method,Name,sizes)
 
-			ids = []
-			response = requests.get(url)
-			content = json.loads(response.text)
-			for i in range(len(content['objects'])):
-				id1 = content['objects'][i]['publishedContent']['properties']['seo']['slug']
-				ids.append(id1)
+                            
+                            old_slugs = new_slugs
+            else:
+                print(str(datetime.now())+ f' ------------------- Monitoring [ {Name} ]')
+                time.sleep(10)
+        except Exception as e:
+            post_log(str(e),Name)
+            print(str(datetime.now()) + f' ----------------------Error Requesting to {Name}')
 
-
-			old_data = open(file1).read()
-
-			difference = [c for c in ids if c not in old_data]
-
-			for i in difference:
-				for c in content['objects']:
-					if c['publishedContent']['properties']['seo']['slug'] == i:
-						try:
-							restricrt = c['publishedContent']['properties']['custom']['restricted']
-						except:
-							restricrt = 'NONE'
-						try:
-							method = c['productInfo'][0]['launchView']['method']
-						except:
-							method ='NONE'
-						try:
-							starttime = c['productInfo'][0]['launchView']['startEntryDate']
-						except:
-							starttime = 'NONE' 
-
-						if restricrt == False:
-
-							restricrt = 'Not Exclusive'
-
-						elif restricrt == True:
-
-							restricrt = 'Exclusive'
-
-						else:
-
-							pass
-
-						try:
-
-							title = c['publishedContent']['properties']['seo']['title']
-
-						except:
-
-							title ='NONE'
-
-						try:
-
-							image = c['publishedContent']['properties']['coverCard']['properties']['squarishURL']
-
-						except:
-
-							image = 'https://pbs.twimg.com/profile_images/1118878674642714624/lNXTIWNT_400x400.jpg'
-
-
-						message_post(webhook_url,title,image,restricrt,method,starttime)
-
-			file = open(file1,'w+')
-
-			file.write(str(ids))
-
-			file.close()
-
-			print(gettime() + "Monitoring>>>>>>>>>")
-
-			time.sleep(2)
-
-		except:
-
-			time.sleep(2)
-
-
-for i in range(len(urls)):
-	(threading.Thread(target=main,args=(urls[i],))).start()
+            
+if __name__ == "__main__":
+    APIS = ['https://api.nike.com/product_feed/threads/v2?filter=marketplace(US)&filter=language(en)&filter=channelId(008be467-6c78-4079-94f0-70e2d6cc4003)&fields=id&fields=lastFetchTime&fields=productInfo&fields=publishedContent.properties','https://api.nike.com/product_feed/threads/v2?filter=marketplace(CN)&filter=language(zh-Hans)&filter=channelId(008be467-6c78-4079-94f0-70e2d6cc4003)&fields=id&fields=lastFetchTime&fields=productInfo&fields=publishedContent.properties','https://api.nike.com/product_feed/threads/v2?filter=marketplace(GB)&filter=language(en-gb)&filter=channelId(008be467-6c78-4079-94f0-70e2d6cc4003)&fields=id&fields=lastFetchTime&fields=productInfo&fields=publishedContent.properties','https://api.nike.com/product_feed/threads/v2?filter=marketplace(JP)&filter=language(ja)&filter=channelId(008be467-6c78-4079-94f0-70e2d6cc4003)&fields=id&fields=lastFetchTime&fields=productInfo&fields=publishedContent.properties']
+    for API in APIS:
+        if 'US' in API:
+            apiName = 'SNKRS US'
+        elif 'CN' in API:
+            apiName = 'SNKRS CN'
+        elif 'GB' in API:
+            apiName = 'SNRKS GB'
+        elif 'JP' in API:
+            apiName = 'SNKRS JP'
+        threading.Thread(target=main,args=(API,apiName,)).start()
